7949d70121ee4ba3bbf769fec039dcc356acbf42
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2016-05-26 09:14:39 +0200

    Make the plugin standalone (*.plugin.zsh can be simply sourced)

--- history-search-multi-word.plugin.zsh
+++ history-search-multi-word.plugin.zsh
@@ -1,3 +1,14 @@
+#
+# No plugin manager is needed to use this file. All that is needed is adding:
+#   source {where-unpacked}/history-search-multi-word.plugin.zsh
+# to ~/.zshrc.
+#
+
+REPO_DIR="${0%/*}"
+if [[ -z "$ZPLG_CUR_PLUGIN" && "${fpath[(r)$REPO_DIR]}" != $REPO_DIR ]]; then
+    fpath+=( "$REPO_DIR" )
+fi
+
 autoload history-search-multi-word
 zle -N history-search-multi-word
 zle -N history-search-multi-word-backwards history-search-multi-word

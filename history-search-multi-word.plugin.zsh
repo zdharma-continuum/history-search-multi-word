64f47cec06a044b433ed7e202b069be08dc7cd1a
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2016-11-03 11:49:07 +0100

    *highlight,*plugin.zsh: Highlight variables when 256 color terminal

--- history-search-multi-word.plugin.zsh
+++ history-search-multi-word.plugin.zsh
@@ -15,5 +15,9 @@ zle -N history-search-multi-word
 zle -N history-search-multi-word-backwards history-search-multi-word
 zle -N history-search-multi-word-pbackwards history-search-multi-word
 zle -N history-search-multi-word-pforwards history-search-multi-word
-[[ "${+termcap[Co]}" = 1 && "${termcap[Co]}" = "256" ]] && zstyle ":history-search-multi-word" highlight-color "bg=17"
+[[ "${+termcap[Co]}" = 1 && "${termcap[Co]}" = "256" ]] && {
+    zstyle ":history-search-multi-word" highlight-color "bg=17"
+    typeset -gA HSMW_HIGHLIGHT_STYLES
+    HSMW_HIGHLIGHT_STYLES[variable]="fg=112"
+}
 bindkey "^R" history-search-multi-word

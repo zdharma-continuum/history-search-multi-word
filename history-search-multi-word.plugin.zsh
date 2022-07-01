458e75c16db72596e4d7c6a45619dec285ebdcd7
vladislav doster <10052309+vladdoster@users.noreply.github.com>
2022-07-01 10:41:59 -0500

    style: format zsh and markdown to zinit convention (#8)
    * style: format zsh & markdown files and update copyright notice
    
    Signed-off-by: Vladislav Doster <mvdoster@gmail.com>

--- history-search-multi-word.plugin.zsh
+++ history-search-multi-word.plugin.zsh
@@ -1,5 +1,9 @@
-# -*- Mode: sh; sh-indentation: 4; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
-# vim:ft=zsh:sw=4:sts=4:et
+#!/usr/bin/env zsh
+#
+# -*- mode: sh; sh-indentation: 2; indent-tabs-mode: nil; sh-basic-offset: 2; -*-
+#
+# Copyright (c) 2016-2020 Sebastian Gniazdowski and contributors
+# Copyright (c) 2021-2022 zdharma-continuum and contributors
 
 #
 # No plugin manager is needed to use this file. All that is needed is adding:
@@ -16,7 +20,7 @@ HSMW_REPO_DIR="${0:h}"
 
 if [[ ${zsh_loaded_plugins[-1]} != */history-search-multi-word && -z ${fpath[(r)${0:h}]} ]]
 then
-    fpath+=( "${0:h}" )
+  fpath+=( "${0:h}" )
 fi
 
 autoload history-search-multi-word hsmw-context-main
@@ -25,10 +29,10 @@ zle -N history-search-multi-word-backwards history-search-multi-word
 zle -N history-search-multi-word-pbackwards history-search-multi-word
 zle -N history-search-multi-word-pforwards history-search-multi-word
 [[ ${+termcap[Co]} = 1 && ${termcap[Co]} = 256 ]] && {
-    zstyle -s ":history-search-multi-word" highlight-color tmp || \
-        zstyle ":history-search-multi-word" highlight-color "bg=17"
-    typeset -gA HSMW_HIGHLIGHT_STYLES
-    [[ ${HSMW_HIGHLIGHT_STYLES[variable]} = none ]] && \
-        HSMW_HIGHLIGHT_STYLES[variable]="fg=112"
+  zstyle -s ":history-search-multi-word" highlight-color tmp || \
+    zstyle ":history-search-multi-word" highlight-color "bg=17"
+  typeset -gA HSMW_HIGHLIGHT_STYLES
+  [[ ${HSMW_HIGHLIGHT_STYLES[variable]} = none ]] && \
+    HSMW_HIGHLIGHT_STYLES[variable]="fg=112"
 }
 bindkey "^R" history-search-multi-word
